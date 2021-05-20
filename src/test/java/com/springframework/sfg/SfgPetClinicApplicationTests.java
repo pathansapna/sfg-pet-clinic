e432a91a0112db21b6c2e2ed4ef7d1beca6552f6
sapna <pathansapna@gmail.com>
2021-05-20 16:13:15 +0530

    Initial Project Add

---
+++ src/test/java/com/springframework/sfg/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
